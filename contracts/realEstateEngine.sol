00f1d320b71c1b9473c158b127bb49f443901a67
ade <harounadelani@gmail.com>
2024-04-29 22:56:19 +0100

    first commit

---
+++ contracts/realEstateEngine.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor () ERC721("RealEstate", "EST") {
+
+    }
+
+    function mint(string memory _tokenURI, address seller) public returns (bool) {
+        uint256 currentId = _tokenIds.current();
+        _mint(seller, currentId);
+        _setTokenURI(currentId, _tokenURI);
+
+        _tokenIds.increment();
+        return true;
+
+        
+    }
+}
